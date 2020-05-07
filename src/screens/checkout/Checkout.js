823ed179d42a1ed85c8328ab80fe93b29355c475
Smeeth Virpariya <smeethvirparia@yahoo.co.in>
2020-05-07 10:55:34 +0530

    Added buttons to stepper to navidate between the steps

--- src/screens/checkout/Checkout.js
+++ src/screens/checkout/Checkout.js
@@ -6,21 +6,57 @@ import './checkout.css'
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
+import StepContent from "@material-ui/core/StepContent";
+import Button from "@material-ui/core/Button";
 
 class Checkout extends Component {
+    constructor() {
+        super();
+        this.state = {
+            activeStep: 0
+        }
+    }
+
     render() {
         return <Fragment>
             <div className='main-container'>
-                <Stepper activeStep='1' orientation='vertical'>
-                    <Step key='Delivery'>
-                        <StepLabel>Delivery</StepLabel>
-                    </Step>
-                    <Step key='Payment'>
-                        <StepLabel>Payment</StepLabel>
-                    </Step>
-                </Stepper>
+                <div className='delivery-payment-section'>
+                    <Stepper activeStep={this.state.activeStep} orientation='vertical'>
+                        <Step key='Delivery'>
+                            <StepLabel>Delivery</StepLabel>
+                            <StepContent>
+                                <div className='address-tabs'>
+                                    <Button style={{margin: 5}} disabled={this.state.activeStep === 0}>Back</Button>
+                                    <Button style={{margin: 5}} className='button' variant="contained" color="primary"
+                                            onClick={this.incrementActiveStep}>Next</Button>
+                                </div>
+                            </StepContent>
+                        </Step>
+                        <Step key='Payment'>
+                            <StepLabel>Payment</StepLabel>
+                            <StepContent>
+                                <Button style={{margin: 5}} onClick={this.decrementActiveStep}>Back</Button>
+                                <Button style={{margin: 5}} variant="contained" color="primary"
+                                        onClick={this.incrementActiveStep}>Finish</Button>
+                            </StepContent>
+                        </Step>
+                    </Stepper>
+                </div>
+                <div className='summary-section'>
+                    <span>Summary</span>
+                </div>
             </div>
         </Fragment>
+    }
+
+    incrementActiveStep = () => {
+        let activeState = this.state.activeStep + 1;
+        this.setState({activeStep: activeState})
+    }
+
+    decrementActiveStep = () => {
+        let activeState = this.state.activeStep - 1;
+        this.setState({activeStep: activeState})
     }
 }
 
