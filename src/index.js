6563c097d8cfae8f29558439b623c630b3c1be94
Gali-Madhan-Kumar <gmadhan99@gmail.com>
2020-05-07 16:41:28 +0530

    created header with logo, search box and login button

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Header from './common/header/Header';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <div>
-        Food Ordering App
-    </div>,
+    <Header />,
     document.getElementById('root')
 );
 
