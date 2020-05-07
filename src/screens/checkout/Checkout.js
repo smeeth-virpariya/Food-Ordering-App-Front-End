3711d1da62e560af246ec163a1bca5ae58c40fa2
Smeeth Virpariya <smeethvirparia@yahoo.co.in>
2020-05-07 21:25:43 +0530

    Added Grid list to display existing addresses.

--- src/screens/checkout/Checkout.js
+++ src/screens/checkout/Checkout.js
@@ -11,6 +11,8 @@ import Button from "@material-ui/core/Button";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import AppBar from "@material-ui/core/AppBar";
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
 
 class Checkout extends Component {
     constructor() {
@@ -20,6 +22,10 @@ class Checkout extends Component {
             activeTabValue: 'existing_address'
         }
     }
+
+    /*componentDidMount() {
+        this.fetchAddress();
+    }*/
 
     render() {
         return <Fragment>
@@ -33,11 +39,51 @@ class Checkout extends Component {
                                     <AppBar position={"relative"}>
                                         <Tabs value={this.state.activeTabValue} variant='standard'>
                                             <Tab value='existing_address' label='EXISTING ADDRESS'
-                                                 onClick={() => this.changeActiveTab('existing_address')}></Tab>
+                                                 onClick={() => this.changeActiveTab('existing_address')}/>
                                             <Tab value='new_address' label='NEW ADDRESS'
-                                                 onClick={() => this.changeActiveTab('new_address')}></Tab>
+                                                 onClick={() => this.changeActiveTab('new_address')}/>
                                         </Tabs>
                                     </AppBar>
+                                </div>
+                                <div id='existing-address-display' className='display-block'>
+                                    <GridList style={{flexWrap: 'nowrap'}} cols={3} cellHeight='auto'>
+                                        <GridListTile>
+                                            <div>
+                                                <p>#307</p>
+                                                <p>Shravanthi Graces, Timappa Reddy Layout, Hulimavu</p>
+                                                <p>Bangalore</p>
+                                                <p>Karnataka</p>
+                                                <p>560076</p>
+                                            </div>
+                                        </GridListTile>
+                                        <GridListTile>
+                                            <div>
+                                                <p>#308</p>
+                                                <p>Shravanthi Graces, Timappa Reddy Layout, Near Matru Nursing Home, Hulimavu, Bannerghatta Main Road,</p>
+                                                <p>Bangalore</p>
+                                                <p>Karnataka</p>
+                                                <p>560076</p>
+                                            </div>
+                                        </GridListTile>
+                                        <GridListTile>
+                                            <div>
+                                                <p>#309</p>
+                                                <p>Shravanthi Graces, Timappa Reddy Layout, Hulimavu</p>
+                                                <p>Bangalore</p>
+                                                <p>Karnataka</p>
+                                                <p>560076</p>
+                                            </div>
+                                        </GridListTile>
+                                        <GridListTile>
+                                            <div>
+                                                <p>#310</p>
+                                                <p>Shravanthi Graces, Timappa Reddy Layout, Hulimavu</p>
+                                                <p>Bangalore</p>
+                                                <p>Karnataka</p>
+                                                <p>560076</p>
+                                            </div>
+                                        </GridListTile>
+                                    </GridList>
                                 </div>
                                 <div>
                                     <Button style={{margin: 5}} disabled={this.state.activeStep === 0}>Back</Button>
@@ -76,6 +122,13 @@ class Checkout extends Component {
     changeActiveTab = (value) => {
         this.setState({activeTabValue: value})
     }
+
+    /*fetchAddress = () => {
+        let url = 'http://localhost:8080/api/address/customer';
+        let accessToken = 'eyJraWQiOiJkOTE5YjNkZS05ZGI4LTQ1MzYtYjQ4My1kNDIxODAxMjE5MzgiLCJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJhdWQiOiIxYTlhZDRjYS04MmMwLTQ3YzQtYTNjZS1kZTcwZTRiZWZjNWEiLCJpc3MiOiJodHRwczovL0Zvb2RPcmRlcmluZ0FwcC5pbyIsImV4cCI6MTU4ODg5MCwiaWF0IjoxNTg4ODYxfQ.bILQoopdMx9Tn7BzgbzgE7toPBXHsmRUJtnw9QSZQpHPhd2F_81d9M7d-PNt-9nytMF62Sz9yZRyJuHiSZ3BCg';
+        let response = get(url, accessToken);
+        console.log(response);
+    }*/
 }
 
 export default Checkout;
