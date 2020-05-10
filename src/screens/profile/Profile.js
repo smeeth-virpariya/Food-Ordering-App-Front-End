d4a210c176ff7eb2f36affdc8d61a8c647621757
Gali-Madhan-Kumar <gmadhan99@gmail.com>
2020-05-10 08:36:33 +0530

    Formatted the code and added comments

--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+
+//importing Header
 import Header from '../../common/header/Header';
 
+//importing the css file of the Profile page
 import './Profile.css';
 
 class Profile extends Component {
@@ -8,6 +11,7 @@ class Profile extends Component {
         return (
             <div>
                 <Header showSearchBox={false} />
+                {/* displays only the text Profile Page */}
                 <div className="page-title">
                     <h1 className="text">Profile Page</h1>
                 </div>
